b0bd99f8223c4a3ec1f6c84d829f6331b7ab3c1d
fatihmobim <fatih@mobimyazilim.com>
2023-12-10 21:17:33 +0300

    socket.io paketi eklendi. Ayrıca online user ve anlık mesaj göndermek için socket.io kullanıldı. Artık Sayfanın refresh yapılmasına gerek kalınmadı.

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express")
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cors = require("cors")
+const io = require("socket.io")(8080,{
+    cors:{
+        origin:"http://localhost:3000"
+    }
+});
 
 //Connect DB
 require("./db/connection")
@@ -20,6 +25,46 @@ const app = express() ;
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors());
+
+//Socket.io
+let users = [];
+
+io.on("connection", socket => {
+
+    socket.on("addUser",userId => {
+        const isUserExist = users.find(user => user.userId === userId);
+        if(!isUserExist){
+            const user = {userId, socketId: socket.id};
+            users.push(user);
+            io.emit("getUsers",users);
+        }
+    });
+
+    socket.on("sendMessage", async ({senderId, reciverId, message, conversationId })=>{
+        const reciver = users.find(user => user.userId === reciverId);
+        const sender = users.find(user => user.userId === senderId);
+        const user = await Users.findById(senderId);
+
+        if(reciver){
+            io.to(reciver.socketId).to(sender.socketId).emit("getMessage",{
+                senderId,
+                message,
+                conversationId,
+                reciverId,
+                user: {id:user._id, fullName : user.fullName, email:user.email }
+            });
+        }
+    });
+
+
+    socket.on("disconnect", () => {
+        users = users.filter(user => user.socketId !== socket.id);
+        io.emit("getUsers",users);
+    });
+
+})
+
+
 
 
 //Routes
